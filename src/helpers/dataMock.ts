b23cfd4dbbc98a8b1449d28b4bc0165fde7ad9b9
victorsheepman <chirlychplin@gmail.com>
2023-04-17 10:08:48 -0400

    add images

--- src/helpers/dataMock.ts
+++ src/helpers/dataMock.ts
@@ -1,27 +1,27 @@
 export const projects = [
  {
-    img:'src/assets/images/blockmaster.jpg',
+    img:'https://trusting-varahamihira-f44907.netlify.app/assets/images/blockmaster.jpg',
     title:'Block Master',
     stack:'React / Sass', 
     github:'https://github.com/victorsheepman/React-BlockMaster',
     web:'https://practical-curie-e6b1e8.netlify.app'
  }, 
  {
-    img:'src/assets/images/Príncipe-fresco.png',
+    img:'https://trusting-varahamihira-f44907.netlify.app/assets/images/Pr%C3%ADncipe-fresco.png',
     title:'Príncipe Fresco',
     stack:'React / Sass', 
     github:'https://github.com/victorsheepman/React-FreshPrince',
     web:'https://keen-sinoussi-15d925.netlify.app/'
  },
  {
-    img:'src/assets/images/Adopción.png',
+    img:'https://trusting-varahamihira-f44907.netlify.app/assets/images/Adopci%C3%B3n.png',
     title:'PetsApp',
     stack:'React / Sass / Mobile First', 
     github:'https://github.com/victorsheepman/React-PetsApp.git',
     web:'https://serene-mccarthy-e98a45.netlify.app/'
  },
  {
-    img:'src/assets/images/Netnet.png',
+    img:'https://trusting-varahamihira-f44907.netlify.app/assets/images/Netnet.png',
     title:'Netnet',
     stack:'JavaScript Vanilla / Sass / Mobile First', 
     github:'https://github.com/victorsheepman/Netnet.git',
